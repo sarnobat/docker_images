27c78a13582fa4648a7f8715ab4d1c7171832cd1
sridhar sarnobat <ss401533@gmail.com>
2022-11-16 15:54:02 -0800

    Minor changes (untested)

--- ubuntu_setup/run_ubuntu.sh
+++ ubuntu_setup/run_ubuntu.sh
@@ -4,27 +4,51 @@ set -e
 #set -o pipefail
 
 cat <<EOF
-/Applications/Docker.app/Contents/MacOS/Docker
-or
-/Applications/Docker.app/Contents/MacOS/com.docker.backend -watchdog -native-api
+First time setup
+-----------------
+so you don't have to use sudo
 EOF
 
-# Get them from inside the mounted computers.git
-rm .zshrc
-rm .aliases
-#cp -v ~/computers.git/docker/.zshrc .
-#cp -v ~/.aliases .
+cat <<EOF > /tmp/docker_config.sh
+sudo usermod -a -G docker sarnobat
+newgrp docker
+EOF
+batcat --plain --paging=never --language sh --theme TwoDark /tmp/docker_config.sh
 
-
+cat <<EOF
+Image building
+--------------
+EOF
 sudo docker rm --force $(sudo docker ps --all --quiet) || echo "no existing container to delete"
 sudo docker image rm --force $(sudo docker images --quiet) || echo "no existing image to delete"
 sudo docker images
 sudo docker build --label mylabel --tag mytag .
 sudo docker images
+
+cat <<EOF
+Container creation & running
+----------------------------
+EOF
+
 # sudo docker run --name mycontainer intro/v1
-sudo docker run --volume /Volumes/git/:/media/sarnobat/git --name mycontainer --detach --tty --interactive mytag zsh 
-sudo docker exec -u root -it mycontainer zsh
+sudo docker run --volume /home/sarnobat/computers.git:/home/root/computers.git -p 2211:22 --name mycontainer --detach --tty --interactive mytag zsh 
+docker exec -u root -it mycontainer zsh
+cat <<EOF
+Cleanup
+-------
+EOF
+cat <<EOF > /tmp/docker_cleanup.sh
 sudo docker stop mycontainer
 sudo docker rm mycontainer
 #sudo docker image rm myimage
 sudo docker ps --all
+
+EOF
+
+cat <<EOF
+sh -x /tmp/docker_cleanup.sh
+EOF
+
+
+batcat --plain --paging=never --language sh --theme TwoDark /tmp/docker_cleanup.sh
+sh -x /tmp/docker_cleanup.sh
