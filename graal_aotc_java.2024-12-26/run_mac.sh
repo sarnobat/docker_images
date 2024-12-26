b47776d4e8d8acfda501721c0b2405f9ee32db1e
Sridhar Sarnobat <sarnobat@mbp2024.local>
2024-12-26 18:45:54 -0500

    Minor changes (untested) 2024-12-26

--- graal_aotc_java.2024-12-26/run_mac.sh
+++ graal_aotc_java.2024-12-26/run_mac.sh
@@ -22,9 +22,15 @@ sudo docker images
 sudo docker build --label mylabel --tag mytag .
 sudo docker images
 # sudo docker run --name mycontainer intro/v1
-sudo docker run --volume /Volumes/git/github/docker_images/graal_aotc_java.2024:/root/proj --name mycontainer_graalvm --detach --tty --interactive mytag sh
-sudo docker exec -u root -it mycontainer_graalvm sh
-sudo docker stop mycontainer
-sudo docker rm mycontainer
+sudo docker run --volume /Volumes/git/github/docker_images/graal_aotc_java.2024:/root/proj --name mycontainer_graalvm --detach --tty --interactive mytag
+docker cp mycontainer_graalvm:/proj/build/native/nativeCompile/graal_aotc_java /tmp/helloworld
+#  sh
+# sudo docker exec -u root -it mycontainer_graalvm sh
+sudo docker stop mycontainer_graalvm
+sudo docker rm mycontainer_graalvm
 #sudo docker image rm myimage
 sudo docker ps --all
+cat <<EOF
+Unfortunately, the native image is only for linux.GraalVM will probably never support cross-compile. You need a MacOS docker base (which is hard to include with GraalVM and Gradle).
+Golang or Github Workflow is the only way solution.
+EOF
