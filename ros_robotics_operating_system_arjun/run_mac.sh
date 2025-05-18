96d5d07b0f563c3f25e8db9c7df581091d84ce4e
Sridhar Sarnobat <ss401533@gmail.com>
2025-05-18 18:46:08 -0400

    2025-05-18: Dockerfile entrypoint.sh run_mac.sh .zshrc Dockerfile

--- ros_robotics_operating_system_arjun/run_mac.sh
+++ ros_robotics_operating_system_arjun/run_mac.sh
@@ -20,10 +20,10 @@ set -e
 sudo docker rm --force $(sudo docker ps --all --quiet) || echo "no existing container to delete"
 sudo docker image rm --force $(sudo docker images --quiet) || echo "no existing image to delete"
 sudo docker images
-sudo docker build --label mylabel --tag mytag .
+sudo docker build --progress=plain  --label mylabel --tag mytag .
 sudo docker images
 # sudo docker run --name mycontainer intro/v1
-sudo docker run --volume /Volumes/git/:/media/sarnobat/git --name mycontainer --detach --tty --interactive mytag zsh 
+sudo docker run --volume /Volumes/git/computers.git:/media/sarnobat/computers.git --name mycontainer --detach --tty --interactive mytag zsh 
 sudo docker exec -u root -it mycontainer zsh
 sudo docker stop mycontainer
 sudo docker rm mycontainer
