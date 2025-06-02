730bc06b197e41224942e0e1bfb7f5c128296b16
Sridhar Sarnobat <ss401533@gmail.com>
2025-06-02 12:40:09 -0400

    2025-06-02: run.sh

---
+++ bison_helloworld/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION			
+# DATE				2025
+# AUTHOR			ss401533@gmail.com
+#----------------------------------------------------------------------------
+# template found at ~/.vim/sh_header.temp
+
+set -o errexit
+echo `date +%s::`"$0" >> ~/db.git/command_history.txt >> ~/db.git/command_history_timestamped.txt
+
+cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
+This makefile is easier to understand. But for latest C code, use /Volumes/git/github/2024/docker_images/bison_helloworld_cmake/src
+EOF
+
