ad23e533649b24655334a59d189d0b68f3543a1d
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 01:17:58 -0400

    2025-07-04: compiler.c.grapheasy.txt.out run.sh

--- bison_helloworld_c/run.sh
+++ bison_helloworld_c/run.sh
@@ -11,6 +11,17 @@ set -o errexit
 echo `date +%s::`"$0" >> ~/db.git/command_history.txt >> ~/db.git/command_history_timestamped.txt
 
 cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
+
++ - - - - - -+     +- - - - - - - - +     + - - - - - - +
+' main.c     '     ' parser.tab.c   '     ' lex.yy.c    '
+'            '     '                '     '             '
+' +--------+ '     ' +------------+ '     ' +---------+ '
+' | main() | ' --> ' | yyparse()  | ' --> ' | yylex() | '
+' +--------+ '     ' +------------+ '     ' +---------+ '
+'            '     '                '     '             '
++ - - - - - -+     +- - - - - - - - +     + - - - - - - +
+
+
 This makefile is easier to understand. But for latest C code, use /Volumes/git/github/2024/docker_images/bison_helloworld_cmake/src which has a bit more bison functionality and run instructions.
 2025-07-04 - I'm not sure this is true anymore. The source files look the same now.
 EOF
