c82f13eed0f1f18ed8aa3f194fe2ec75fa46f304
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 01:05:00 -0400

    2025-07-04: compiler.grapheasy.txt.out helloworld.l helloworld.y main.c

--- bison_helloworld_c_cmake/src/main.c
+++ bison_helloworld_c_cmake/src/main.c
@@ -4,6 +4,10 @@ extern int yyparse(void);
 
 int main(void) {
 	printf("[debug] %s:%d main()\n", __FILE__,  __LINE__);
-	return yyparse();
+
+	fprintf(stderr, "[debug] %s:%d\tgoing to call yyparse()\n", __FILE__,  __LINE__);
+	int status = yyparse();
+	fprintf(stderr, "[debug] %s:%d\treturned from yyparse()\n", __FILE__,  __LINE__);
+	return status;
 }
 
