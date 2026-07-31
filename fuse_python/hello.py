0a3b3aca6d22c369e1700b64f5e8ec60233c66a8
Sridhar Sarnobat <ss401533@gmail.com>
2026-07-30 21:29:42 -0500

    2026-07-30: Dockerfile Makefile ganesha.conf hello.py

--- fuse_python/hello.py
+++ fuse_python/hello.py
@@ -1,12 +1,15 @@
 from fuse import FUSE, Operations
 import os
+import sys
+
+HELLO_CONTENT = b"[hello.py] read()\n"
 
 class HelloFS(Operations):
     def getattr(self, path, fh=None):
         if path == '/':
             return dict(st_mode=(0o755 | 0o040000), st_nlink=2)
         elif path == '/hello':
-            return dict(st_mode=(0o444 | 0o100000), st_nlink=1, st_size=len("[hello.py] getattr()\n"))
+            return dict(st_mode=(0o444 | 0o100000), st_nlink=1, st_size=len(HELLO_CONTENT))
         else:
             raise FileNotFoundError
 
@@ -20,9 +23,9 @@ class HelloFS(Operations):
 
     def read(self, path, size, offset, fh):
         if path == '/hello':
-            data = b"[hello.py] read()\n"
-            return data[offset:offset + size]
+            return HELLO_CONTENT[offset:offset + size]
         raise FileNotFoundError
 
 if __name__ == '__main__':
-    FUSE(HelloFS(), '/mnt/fuse', foreground=True, allow_other=True)
+    mountpoint = sys.argv[1] if len(sys.argv) > 1 else '/mnt/fuse'
+    FUSE(HelloFS(), mountpoint, foreground=True, allow_other=True)
